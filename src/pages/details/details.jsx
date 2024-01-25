dd29af6c6364e6ec154bb958bd50934c485fdf89
ChuzaWick420 <zaiddadev@gmail.com>
2024-01-25 17:58:17 +0500

    fills up details page (except for footer)

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import getData from "../../api/get";
 
 function Details () {
 
     let loc = useLocation();
     let current_id = loc.pathname.split("/")[2];
 
-    const [activeArticleID, setActiveArticleID] = useState(current_id);
+    const[heading, setHeading] = useState("NULL");
+    const[icon, setIcon] = useState("");
+    const[content, setContent] = useState("null");
+
+    useEffect(()=>{
+        getData("details", current_id).then((data)=>{
+            setHeading(data.heading);
+            setIcon(data.icon_url);
+            setContent(data.content);
+        });
+    }, []);
 
     return (
         <div>
             <div className="details_header">
-                <h1></h1>
-                <img />
+                <h1>{heading}</h1>
+                <img src={icon} />
             </div>
             <div className="details_content">
-
+                {content}
             </div>
             <footer>
                 
