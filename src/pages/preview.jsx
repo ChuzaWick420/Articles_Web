0bbbb68f121558781af3a7f881f5946fe860b32a
ChuzaWick420 <zaiddadev@gmail.com>
2024-01-25 15:02:33 +0500

    checking for passed parameters in preview to confirm routes are working fine
    Send the article_id passed from the route and shows it on the screen to confirm if we are at the index or a particular id

--- src/pages/preview.jsx
+++ src/pages/preview.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function Preview () {
+
+    const current_article_id = useParams();
+
     return (
-        <div>Welcome to previews</div>
+        <div>Welcome to previews: {current_article_id.article_id} </div>
     );
 }
 
