05ce8c6bc21f08ad589eae236088886c223ad025
ChuzaWick420 <zaiddadev@gmail.com>
2024-01-25 17:25:59 +0500

    Added structure of details page

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Details () {
+
+    let loc = useLocation();
+    let current_id = loc.pathname.split("/")[2];
+
+    const [activeArticleID, setActiveArticleID] = useState(current_id);
+
     return (
-        <div>Details page</div>
+        <div>
+            <div className="details_header">
+                <h1></h1>
+                <img />
+            </div>
+            <div className="details_content">
+
+            </div>
+            <footer>
+                
+            </footer>
+        </div>
     );
 }
 
