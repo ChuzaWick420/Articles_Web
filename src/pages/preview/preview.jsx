4bb2e7e7f797cdfd1e18c16365073ec209461c26
ChuzaWick420 <zaiddadev@gmail.com>
2024-01-29 13:09:24 +0500

    Added: link to current article id details and added structure for the main section of the preview page

--- src/pages/preview/preview.jsx
+++ src/pages/preview/preview.jsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Preview () {
 
     const para_article_id = useParams();
     const [activeArticleID, setActiveArticleID] = useState(0);
+    const [activeCategoryID, setActiveCategoryID] = useState(0);
 
     useEffect(()=>{
         setActiveArticleID(para_article_id);
     }, []);
 
     return (
-        <div>Welcome to previews: {activeArticleID.article_id} </div>
+        <div className="preview_page">
+            <div className="main">
+                <div className="preview_header">
+                    <h1></h1>
+                    <img></img>
+                </div>
+                <Link to = {`../preview/${para_article_id.article_id}/details`}>
+                    <button>Read More</button>
+                </Link>
+                <footer className="preview_footer"></footer>
+            </div>
+            <div className="sub"></div>
+        </div>
     );
 }
 
